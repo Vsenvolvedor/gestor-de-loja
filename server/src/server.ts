cc7e6a034877873d744e73f2a0176cee18b76d6e
Vitor <catchtherainbow33@gmail.com>
2022-06-23 23:24:38 -0300

    Server: Arquivo de server refatorado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,20 @@
-import express from 'express'
-import cors from 'cors' 
-import store  from './routes/store' 
-import user from './routes/user' 
-import categ from './routes/categ' 
-const app = express()
+import express from 'express';
+import cors from 'cors';
+import store  from './routes/store';
+import user from './routes/user';
+import categ from './routes/categ'; 
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/product', store)
-app.use('/api/user', user)
-app.use('/api/categ', categ)
+const app = express();
 
-app.listen(5555, () => {
-  console.log('RODANDO')
-  console.log('http://localhost:5555')
-})
+app.use(cors());
+app.use(express.json());
+app.use('/api/product', store);
+app.use('/api/user', user);
+app.use('/api/categ', categ);
+
+
+const port = 5555;
+app.listen(port, () => {
+  console.log('RODANDO');
+  console.log(`http://localhost:${port}`);
+});
