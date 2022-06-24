e176607c63e17eadc62be0d81e78ed43d4b12642
Vitor <catchtherainbow33@gmail.com>
2022-06-23 23:48:41 -0300

    Categ route refatorada

--- server/src/controllers/deleteCateg.ts
+++ server/src/controllers/deleteCateg.ts
@@ -2,7 +2,13 @@ import Database from '../db/config'
 import { createResponse } from '../helpers/createResponse'
 
 export async function deleteCateg(id:number, categName:string) {
-  const db = await Database
-  db.run(`DELETE FROM categories WHERE ownerID=${id} AND name="${categName}"`)
-  return createResponse(200,'Produto deletado')
+  try {
+    const db = await Database
+    db.run(`DELETE FROM categories WHERE ownerID=${id} AND name="${categName}"`)
+
+    
+    return createResponse(200,'Produto deletado')
+  } catch (err) {
+    return createResponse(404,'Produto não encontrado')
+  }
 }
--- server/src/routes/categ.ts
+++ server/src/routes/categ.ts
@@ -1,52 +1,50 @@
-import express from 'express'
-import { createCateg } from '../controllers/createCateg'
-import { deleteCateg } from '../controllers/deleteCateg'
-import { getCategs } from '../controllers/getCategs'
-import { createResponse } from '../helpers/createResponse'
-import { isLogged } from '../middleware/isLogged'
+import express, { Request, Response } from 'express';
+import { createCateg } from '../controllers/createCateg';
+import { deleteCateg } from '../controllers/deleteCateg';
+import { getCategs } from '../controllers/getCategs';
+import { createResponse } from '../helpers/createResponse';
+import { isLogged } from '../middleware/isLogged';
 
-const categData = async (req:any,res:any) => {
+const categData = async (req:Request,res:Response) => {
   try {
-    const { ID } = res.locals.userData
-    const response = await getCategs(ID)
+    const { ID } = res.locals.userData;
+    const response = await getCategs(ID);
   
     if(response) {
-     res.status(response.status).json(response)
-    } else throw new Error()
+     res.status(response.status).json(response);
+    } else throw new Error();
   } catch(err) {
-    return createResponse(400, 'Não foi possivel carregar as categorias')
-  }
-}
+    return createResponse(400, 'Não foi possivel carregar as categorias');
+  };
+};
 
-const categCreate = async (req:any,res:any) => {
+const categCreate = async (req:Request,res:Response) => {
   try {
-    const { ID, storename } = res.locals.userData
-    const { name } = req.body
+    const { ID, storename } = res.locals.userData;
+    const { name } = req.body;
   
-    const response = await createCateg(ID, storename, name)
+    const response = await createCateg(ID, storename, name);
 
     if(response) {
-      res.status(response.status).json(response)
-    } else throw new Error()
+      res.status(response.status).json(response);
+    } else throw new Error();
   } catch(err) {
-    return createResponse(400, 'Não foi possivel criar a categoria')
+    return createResponse(400, 'Não foi possivel criar a categoria');
   }
-  
-}
+};
 
-const categDelete = async (req:any,res:any) => {
+const categDelete = async (req:Request,res:express.Response) => {
   try {
-    const { ID } = res.locals.userData
-    const { name } = req.params
+    const { ID } = res.locals.userData;
+    const { name } = req.params;
   
-    const response = await deleteCateg(ID, name)
+    const response = await deleteCateg(ID, name);
     if(response) {
-      res.status(response.status).json(response)
-    } else throw new Error()
-  } catch(err) {
-    res.status(400).send('Categoria não encontrada')
+      res.status(response.status).json(response);
+    } else throw new Error('Categoria não encontrada');
+  } catch(err:any) {
+    res.status(400).send(err.message);
   }
- 
 }
 
 
